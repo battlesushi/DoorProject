30ec3c181f297079c89053b792c7fa6e288f23b4
battlesushi <105021030@live.asia.edu.tw>
2019-06-19 16:48:38 +0800

    6/19 frozen && register_function && record

--- src/DoorApp/tcplistenToDB.java
+++ src/DoorApp/tcplistenToDB.java
@@ -12,7 +12,9 @@ import javax.swing.SwingUtilities;
 
 public class tcplistenToDB {
 	private static String rename;
+	private static String retype;
     protected static String ok = "3";
+    private static boolean checkID = false;
     String tcpcardNumber=newtcplisten.tcpcardNumber;
     String driver = "com.mysql.jdbc.Driver";
     String url = "jdbc:mysql://120.108.111.137:3306/105021043?characterEncoding=utf-8";
@@ -25,60 +27,79 @@ public class tcplistenToDB {
             Connection con = DriverManager.getConnection(url, user, passwd);
             Statement st = con.createStatement();
 
-            
-                String s = " SELECT * FROM Member";
+                String s = " SELECT * FROM Member";//Member
                 PreparedStatement ps = con.prepareStatement(s);
                 ResultSet rs = ps.executeQuery();
-                while (rs.next()) {
-                    if (tcpcardNumber.substring(32, 43).equals(rs.getString("ID"))) {
-                        ok = "1";
-                        rename = rs.getString("Name");
-                        SwingUtilities.invokeLater(new Runnable()////��?��?����?����?�����??��蹎��?�����??��謘extField���蕭??��
-                        {
-                            public void run()
-                            {
-//                                tcpcardNumber="";
-                            }
-                        });
-                        tcpTestClass tc=new tcpTestClass();
-                        tc.run();
-//                        showPass.setForeground(Color.BLUE);
-//                        showPass.setText("??��?�蕭��?��?��?�����??�蕭謍�?�蕭謍�?��?��?��??��?��??�??��?��?��?��?��??��蕭!!");
-//                        TestClass tc = new TestClass();
-//                        tc.run();
-//                        try{
-//                        	Thread.sleep(1000);
-//                        	showPass.setText("");//消除??�示
-//                        }catch(Exception e){}
+                while(rs.next()) {
+                	if (tcpcardNumber.substring(32, 43).equals(rs.getString("ID"))&&rs.getString("Type").equals("已凍結")) {
+                		rename = rs.getString("Name");
+                		SimpleDateFormat sdFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm");
+                        Date date = new Date();
+                        String strDate = sdFormat.format(date);
+                        String s2 = "INSERT INTO Record (Name,Time,State,ID) VALUES(?,?,'已凍結','"+tcpcardNumber.substring(32, 43)+"')";
+                        PreparedStatement ps2 = con.prepareStatement(s2);
+                        ps2.setString(1, rename);
+                        ps2.setString(2, strDate);
+                        ps2.executeUpdate();
+                        checkID = true;
                         break;
-                    }
+                	}         
+
                     
                     
-                }
-                if (ok.equals("1")) {
-                    SimpleDateFormat sdFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm");
-                    Date date = new Date();
-                    String strDate = sdFormat.format(date);
-                    String s2 = "INSERT INTO Record (Name,Time,State,ID) VALUES(?,?,'已註冊','"+tcpcardNumber.substring(32, 43)+"')";
-                    PreparedStatement ps2 = con.prepareStatement(s2);
-                    ps2.setString(1, rename);
-                    ps2.setString(2, strDate);
-                    ps2.executeUpdate();
-                    ok="3";
-                } else {
-                	SwingUtilities.invokeLater(new Runnable()////��?��?����?����?�����??��蹎��?�����??��謘extField���蕭??��
+                	else if (tcpcardNumber.substring(32, 43).equals(rs.getString("ID"))&& rs.getString("Type").equals("已註冊")) {
+                            ok = "1";
+                            rename = rs.getString("Name");
+                            SwingUtilities.invokeLater(new Runnable()////��?��?����?����?�����??��蹎��?�����??��謘extField���蕭??��
                             {
                                 public void run()
                                 {
 //                                    tcpcardNumber="";
                                 }
                             });
-//                    showPass.setForeground(Color.RED);
-//                    showPass.setText("??��?�蕭��?��?����?��?��?��?��?�����蕭��蝴���??��?�蕭���??�?��?��??��?��??��?��?��?��?�蕭蹎刻?��??���?�蕭謕�?�!!");
+                            	tcpTestClass tc=new tcpTestClass();
+                                tc.run();
+                                SimpleDateFormat sdFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm");
+                                Date date = new Date();
+                                String strDate = sdFormat.format(date);
+                                String s4 = "INSERT INTO Record (Name,Time,State,ID) VALUES(?,?,'已註冊','"+tcpcardNumber.substring(32, 43)+"')";
+                                PreparedStatement ps4 = con.prepareStatement(s4);
+                                ps4.setString(1, rename);
+                                ps4.setString(2, strDate);
+                                ps4.executeUpdate();
+                                ok="3";
+                                checkID = true;
+//                            showPass.setForeground(Color.BLUE);
+//                            showPass.setText("??��?�蕭��?��?��?�����??�蕭謍�?�蕭謍�?��?��?��??��?��??�??��?��?��?��?��??��蕭!!");
+//                            TestClass tc = new TestClass();
+//                            tc.run();
+//                            try{
+//                            	Thread.sleep(1000);
+//                            	showPass.setText("");//消除??�示
+//                            }catch(Exception e){}
+                            break;
+                        }   
+                    
+                	
                 }
+            	if(checkID==false) {
+            		if(!tcpcardNumber.substring(32, 43).equals(" (M16)Egres")) {
+            			SimpleDateFormat sdFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm");
+                        Date date = new Date();
+                        String strDate = sdFormat.format(date);
+                        String s2 = "INSERT INTO Record (Name,Time,State,ID) VALUES('Unknown',?,'未註冊','"+tcpcardNumber.substring(32, 43)+"')";
+                        PreparedStatement ps2 = con.prepareStatement(s2);
+                        ps2.setString(1, strDate);
+                        ps2.executeUpdate();
+                        
+            		}	
+            	}                       
+
+
             
             con.close();
         } catch (Exception e) {
+//        	System.out.print(e);
         }
     	
     }
